c6769ae9026aff16f466a75f760af4c5366d32c8
Michael Maloof <michaelmaloof1991@gmail.com>
2014-11-03 15:24:06 -0600

    profile for passengers set up
    Signed-off-by: Michael Maloof <michaelmaloof1991@gmail.com>

---
+++ PersonalDriver/PassengerProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserProfileViewController.h
+//  PersonalDriver
+//
+//  Created by Michael Maloof on 11/3/14.
+//  Copyright (c) 2014 TeamPersonalDriver. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface UserProfileViewController : ViewController
+
+@end
