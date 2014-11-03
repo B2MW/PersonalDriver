b581ca802f4b90e4433fcf96d63556e6bf8d2315
Michael Maloof <michaelmaloof1991@gmail.com>
2014-11-03 15:43:03 -0600

    request new ride view controller set up
    Signed-off-by: Michael Maloof <michaelmaloof1991@gmail.com>

---
+++ NewRideViewController.m
@@ -0,0 +1,30 @@
+//
+//  NewRideViewController.m
+//  PersonalDriver
+//
+//  Created by Michael Maloof on 11/3/14.
+//  Copyright (c) 2014 TeamPersonalDriver. All rights reserved.
+//
+
+#import "NewRideViewController.h"
+
+@interface NewRideViewController ()
+
+@end
+
+@implementation NewRideViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self.view endEditing:YES];
+
+}
+
+
+
+@end
