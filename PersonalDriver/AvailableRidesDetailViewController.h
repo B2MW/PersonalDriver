78fa1d0b773c2616307557ae54c8548b51a827b4
Bradley <bradley.m.walker@gmail.com>
2014-11-04 06:29:34 -0600

    Added custom classes for available rides tableview and detail view

---
+++ PersonalDriver/AvailableRidesDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  AvailableRidesDetailViewController.h
+//  PersonalDriver
+//
+//  Created by Bradley Walker on 11/4/14.
+//  Copyright (c) 2014 TeamPersonalDriver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AvailableRidesDetailViewController : UIViewController
+
+@end
---
+++ PersonalDriver/AvailableRidesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AvailableRidesTableViewController.h
+//  PersonalDriver
+//
+//  Created by Bradley Walker on 11/4/14.
+//  Copyright (c) 2014 TeamPersonalDriver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AvailableRidesTableViewController : UITableViewController
+
+@end
