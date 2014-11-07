fd23566dab738d7b3c9b73cbbb77652f8b48e2c1
Bradley <bradley.m.walker@gmail.com>
2014-11-07 10:21:08 -0600

    In process of updating Driver UX

--- PersonalDriver/AvailableRidesDetailViewController.m
+++ PersonalDriver/AvailableRidesDetailViewController.m
@@ -12,58 +12,71 @@
 @property (strong, nonatomic) IBOutlet UIImageView *passengerPicture;
 @property (strong, nonatomic) IBOutlet UILabel *passengerName;
 @property (strong, nonatomic) IBOutlet UILabel *estimatedFare;
+@property (strong, nonatomic) IBOutlet UILabel *rideDate;
 @property (strong, nonatomic) IBOutlet UILabel *numberOfPassengers;
+@property (strong, nonatomic) IBOutlet UIView *pickupAddressLabel;
 @property (strong, nonatomic) IBOutlet UITextView *pickupAddress;
+@property (strong, nonatomic) IBOutlet UILabel *dropoffAddressLabel;
 @property (strong, nonatomic) IBOutlet UITextView *dropoffAddress;
-
+@property (strong, nonatomic) IBOutlet UILabel *specialInstructionsLabel;
+@property (strong, nonatomic) IBOutlet UITextView *specialInstructions;
 @end
 
 @implementation AvailableRidesDetailViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-//    [self refreshDisplay];
-    [self getUserImage];
+- (void)viewWillAppear:(BOOL)animated
+{
+    [self retrieveAvailableRidesData];
 }
 
--(void)getUserImage
-{
-//    NSData *pictureData = [NSData new];
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
 
-    PFQuery *queryForUserImage = [PFQuery queryWithClassName:@"_User"];
-    [queryForUserImage whereKey:@"objectId" equalTo:self.ride.passenger.objectId];
-    [queryForUserImage findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+-(void)retrieveAvailableRidesData
+{
+    RideManager *rideManager = [[RideManager alloc] init];
+    PFQuery *queryForUserDetails = [PFQuery queryWithClassName:@"_User"];
+    [queryForUserDetails whereKey:@"objectId" equalTo:self.ride.passenger.objectId];
+    [queryForUserDetails findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error)
+    {
         if (error || nil)
         {
             NSLog(@"Error: %@", error.userInfo);
-            self.passengerPicture.image = [UIImage imageNamed:@"passengerPicPlaceholder"];
         }
         else
         {
-            [objects.firstObject[@"picture"] getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
-                if (!error) {
-                    self.passengerPicture.image = [UIImage imageWithData:data];
-                    [self refreshDisplay];
+            [objects.firstObject[@"picture"] getDataInBackgroundWithBlock:^(NSData *data, NSError *error)
+            {
+                if (!error)
+                {
+                    if (data != nil)
+                    {
+                        self.passengerPicture.image = [UIImage imageWithData:data];
+                    }
+                    else
+                    {
+                        self.passengerPicture.image = [UIImage imageNamed:@"passengerPicPlaceholder"];
+                    }
                 }
             }];
-
-//            PFFile *image = [PFFile fileWithData:objects.firstObject[@"picture"]];
-//            NSData *data = [NSData dataWithData:image];
         }
+        self.passengerName.text = objects.firstObject[@"name"];
+        self.estimatedFare.text = [rideManager formatRideFareEstimate:self.ride.fareEstimateMin :self.ride.fareEstimateMax];
+        self.rideDate.text = [rideManager formatRideDate:self.ride];
+        self.numberOfPassengers.text = self.ride.passengerCount;
     }];
 }
 
--(void)refreshDisplay
-{
-    RideManager *rideManager = [[RideManager alloc] init];
-//    self.passengerPicture.image = [UIImage imageNamed:@"passengerPicPlaceholder"];
-    self.passengerName.text = @"Passenger's Name";
-    self.numberOfPassengers.text = self.ride.passengerCount;
-    self.estimatedFare.text = [rideManager formatRideFareEstimate:self.ride.fareEstimateMin :self.ride.fareEstimateMax];
-    self.numberOfPassengers.text = self.ride.passengerCount;
-    self.pickupAddress.text = @"111 S Wacker Dr\nChicago, IL 60606";
-    self.dropoffAddress.text = @"848 W Montrose Ave\nChicago, IL 60616";
-}
+//- (void)unhideRideDetails
+//{
+//    self.pickupAddressLabel.hidden = NO;
+//    self.pickupAddress.hidden = NO;
+//    self.dropoffAddressLabel = NO;
+//    self.dropoffAddress = NO;
+//    self.specialInstructionsLabel = NO;
+//    self.specialInstructions = NO;
+//}
 
 - (IBAction)onScheduleRideButtonPressed:(id)sender
 {
