b4df265ff5972617be85d577da420665972e4705
Bradley <bradley.m.walker@gmail.com>
2014-11-11 12:26:05 -0600

    Added distance and bearing calculation methods for available rides

--- PersonalDriver/RideManager.m
+++ PersonalDriver/RideManager.m
@@ -8,6 +8,11 @@
 
 #import "RideManager.h"
 
+@interface RideManager()
+@property CLLocationManager *locationManager;
+
+@end
+
 
 @implementation RideManager
 
@@ -68,9 +73,60 @@
      }];
 }
 
--(void)retrieveRideDistanceAndBearing:(Ride *)ride
+-(void)retrieveRideDistanceAndBearing:(Ride *)ride:(void(^)(NSArray *))completionHandler
 {
+    NSArray *distanceAndBearing = [NSArray array];
+    NSNumber *distance;
+    NSNumber *bearing;
+    RideManager *rideManager = [RideManager new];
 
+    [self.locationManager startUpdatingLocation];
+
+    //find distance between pickup & dropoff GeoPoints
+    CLLocation *pickupLocation = [[CLLocation alloc] initWithLatitude:ride.pickupGeoPoint.latitude longitude:ride.pickupGeoPoint.longitude];
+    CLLocation *driverLocation = self.locationManager.location;
+    distance = [NSNumber numberWithDouble:([pickupLocation distanceFromLocation:driverLocation] / 1609.34)];
+
+    NSNumber *driverLat = [self convertDegreesToRadians:driverLocation.coordinate.latitude];
+    NSNumber *driverLong = [self convertDegreesToRadians:driverLocation.coordinate.longitude];
+    NSNumber *pickupLat = [self convertDegreesToRadians:pickupLocation.coordinate.latitude];
+    NSNumber *pickupLong = [self convertDegreesToRadians:pickupLocation.coordinate.longitude];
+
+    NSNumber *degree = [self convertRadiansToDegrees:(atan2(sin(pickupLong.doubleValue-driverLong.doubleValue)*cos(pickupLat.doubleValue), cos(driverLat.doubleValue)*sin(pickupLat.doubleValue)-sin(driverLat.doubleValue)*cos(pickupLat.doubleValue)*cos(pickupLong.doubleValue-driverLong.doubleValue)))];
+
+    if (degree >= 0) {
+        bearing = degree;
+    } else {
+        bearing = @(360.0 + degree.doubleValue);
+    }
+
+    [distanceAndBearing arrayByAddingObject:distance];
+    [distanceAndBearing arrayByAddingObject:bearing];
+    NSLog(@"distance is %@ miles", distance);
+    NSLog(@"bearing is %@ degrees", bearing);
+}
+
+-(NSNumber *)convertDegreesToRadians:(double)valueInDegrees
+{
+    NSNumber *radianValue = [NSNumber numberWithDouble:(M_PI * valueInDegrees / 180.0)];
+    return radianValue;
+}
+
+-(NSNumber *)convertRadiansToDegrees:(double )valueInRadians
+{
+    NSNumber *degreeValue = [NSNumber numberWithDouble:(valueInRadians * 180.0 / M_PI)];
+    return degreeValue;
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    for (CLLocation *location in locations) {
+        if (location.verticalAccuracy < 1000 && location.horizontalAccuracy < 1000)
+        {
+            [self.locationManager stopUpdatingLocation];
+            break;
+        }
+    }
 }
 
 @end
