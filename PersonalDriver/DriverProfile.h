c69b8d15834cf57ce340e5983b6ed2817f36c445
Bradley <bradley.m.walker@gmail.com>
2014-11-12 14:44:18 -0600

    Added DriverProfile classes

---
+++ PersonalDriver/DriverProfile.h
@@ -0,0 +1,13 @@
+//
+//  DriverProfile.h
+//  PersonalDriver
+//
+//  Created by Bradley Walker on 11/12/14.
+//  Copyright (c) 2014 TeamPersonalDriver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DriverProfile : NSObject
+
+@end
---
+++ PersonalDriver/DriverProfile.m
@@ -0,0 +1,13 @@
+//
+//  DriverProfile.m
+//  PersonalDriver
+//
+//  Created by Bradley Walker on 11/12/14.
+//  Copyright (c) 2014 TeamPersonalDriver. All rights reserved.
+//
+
+#import "DriverProfile.h"
+
+@implementation DriverProfile
+
+@end
