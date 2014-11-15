4a3d62d89052f484a05fa8d1681f81f0a5ba1151
Pete McCarthy <petemccarthy@gmail.com>
2014-11-15 16:42:48 -0600

    clear badge count
    Signed-off-by: Pete McCarthy <petemccarthy@gmail.com>

--- PersonalDriver/AppDelegate.m
+++ PersonalDriver/AppDelegate.m
@@ -105,7 +105,11 @@
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    if (currentInstallation.badge != 0) {
+        currentInstallation.badge = 0;
+        [currentInstallation saveEventually];
+    }
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
